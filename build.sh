158f3d0a8be35148f3e3efbc124c72612dc164a2
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-04-26 20:15:11 +0100

    feat: add logging

--- build.sh
+++ build.sh
@@ -21,6 +21,7 @@ fi
 # Node.js Docker example.
 docker build -t release-test:latest --progress plain ./node-docker-example/
 # Login.
+echo "Logging in as" ${GITHUB_USERNAME}
 echo ${GITHUB_TOKEN} | docker login ghcr.io -u ${GITHUB_USERNAME} --password-stdin
 # Tag.
 docker tag release-test:latest ghcr.io/a-h/release-test:latest
