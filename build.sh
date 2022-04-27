96db5d5f25a7c1d586d858199210137688ead291
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-04-27 18:35:52 +0100

    feat: build a CDK Docker image

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ fi
 export REPO_OWNER="a-h"
 export REPO_NAME="release-test"
 
-# Node.js Docker example.
+# Build Node.js Docker example.
 docker build -t ${REPO_NAME}:latest --progress plain ./node-docker-example/
 # Login.
 echo "Logging in as" ${GITHUB_USERNAME}
@@ -30,4 +30,14 @@ echo ${GITHUB_TOKEN} | docker login ghcr.io -u ${GITHUB_USERNAME} --password-std
 # Tag.
 docker tag ${REPO_NAME}:latest ghcr.io/${REPO_OWNER}/${REPO_NAME}:latest
 docker tag ${REPO_NAME}:latest ghcr.io/${REPO_OWNER}/${REPO_NAME}:${VERSION}
+docker push --all-tags ghcr.io/${REPO_OWNER}/${REPO_NAME}
+
+# Set up CDK.
+docker build -t ${REPO_NAME}:cdk-latest --progress plain ./cdk/
+# Login.
+echo "Logging in as" ${GITHUB_USERNAME}
+echo ${GITHUB_TOKEN} | docker login ghcr.io -u ${GITHUB_USERNAME} --password-stdin
+# Tag.
+docker tag ${REPO_NAME}:cdk-latest ghcr.io/${REPO_OWNER}/${REPO_NAME}:cdk-latest
+docker tag ${REPO_NAME}:cdk-latest ghcr.io/${REPO_OWNER}/${REPO_NAME}:cdk-${VERSION}
 docker push --all-tags ghcr.io/${REPO_OWNER}/${REPO_NAME}
