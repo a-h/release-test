44ef3248df56f8f4674be4fe9ce8b4e6e41f4510
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-04-27 18:03:11 +0100

    feat: add logging to show owner, and name

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 set -e
 
 # Check environment.
-if [ "${REPO_NAME}" = "" ]
-then
-	echo "Missing REPO_NAME"
-	exit 1
-fi
 if [ "${REPO_OWNER}" = "" ]
 then
 	echo "Missing REPO_OWNER"
+	exit 1
+fi
+if [ "${REPO_NAME}" = "" ]
+then
+	echo "Missing REPO_NAME"
 	exit 1
 fi
 if [ "${VERSION}" = "" ]
@@ -19,4 +19,8 @@ then
 fi
 
 # Deploy.
+echo Running CDK deploy
+echo REPO_OWNER=${REPO_OWNER} 
+echo REPO_NAME=${REPO_NAME} 
+echo VERSION=${VERSION} 
 cd cdk && cdk deploy --require-approval=never
