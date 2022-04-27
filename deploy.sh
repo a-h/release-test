a5fcb040f8086fff9fd6d4dd47284650b918f96f
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-04-27 20:57:52 +0100

    feat: pass through the AWS environment

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,16 @@ then
 	echo "Missing VERSION"
 	exit 1
 fi
+if [ "${AWS_SESSION_TOKEN}" = "" ]
+then
+	echo "Missing AWS_SESSION_TOKEN - is a role logged in?"
+	exit 1
+fi
 
 # Deploy.
 echo Running CDK deploy
 echo REPO_OWNER=${REPO_OWNER} 
 echo REPO_NAME=${REPO_NAME} 
 echo VERSION=${VERSION} 
-docker run -e REPO_OWNER -e REPO_NAME -e VERSION ghcr.io/${REPO_OWNER}/${REPO_NAME}:cdk-${VERSION} deploy --require-approval=never
+echo `env`
+docker run -e AWS_SESSION_TOKEN -e REPO_OWNER -e REPO_NAME -e VERSION ghcr.io/${REPO_OWNER}/${REPO_NAME}:cdk-${VERSION} deploy --require-approval=never
